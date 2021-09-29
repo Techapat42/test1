1ea97c52d4bfbb670a93c58163ec7d92e3b66046
sutiphongbeom <sutiphog_saoram@outlook.co.th>
2021-09-29 22:16:02 +0700

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+   
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <!-- Fonts -->
+ <link rel="stylesheet" href={{asset('assets/bootstrap/css/bootstrap.css')}}>
+    <script src= {{asset('assets/bootstrap/js/bootstrap.bundle.min.js')}}></script>
+    <script src="{{asset('assets/jquery/jquery-3.6.0.min.js')}}"></script>
+    </head>
+    <body class="">
+     <div class="container">
+         <p>TestText</p>
+     </div>
+    </body>
+</html>
